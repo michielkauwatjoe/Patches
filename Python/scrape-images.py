cce8516e72fb324880b8f83daaa4c0e7d1c2bc96
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2012-12-09 11:05:53 +0100

    Working on a script to fetch all linked images from a blog.

---
+++ Python/scrape-images.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+#
+# Downloads all the image links at the supplied URL, and saves them to the
+# specified output file ("/test/" by default)
+#
+# Usage:
+#
+# python scrape-images.py http://example.com/ [output]
+#
+
+from BeautifulSoup import BeautifulSoup as bs
+import urlparse
+from urllib2 import urlopen
+from urllib import urlretrieve
+import os
+import sys
+
+def main(url, out_folder="/test/"):
+    u"""
+	Downloads all the images at URL to /test/
+	"""
+    soup = bs(urlopen(url))
+    parsed = list(urlparse.urlparse(url))
+
+    for image in soup.findAll("img"):
+        print "Image: %(src)s" % image
+        filename = image["src"].split("/")[-1]
+        parsed[2] = image["src"]
+        outpath = os.path.join(out_folder, filename)
+        if image["src"].lower().startswith("http"):
+            urlretrieve(image["src"], outpath)
+        else:
+            urlretrieve(urlparse.urlunparse(parsed), outpath)
+
+def _usage():
+	u"""
+	"""
+    print "usage: python dumpimages.py http://example.com [outpath]"
+
+if __name__ == "__main__":
+	u"""
+	"""
+    url = sys.argv[-1]
+    out_folder = "/test/"
+
+    if not url.lower().startswith("http"):
+        out_folder = sys.argv[-1]
+        url = sys.argv[-2]
+        if not url.lower().startswith("http"):
+            _usage()
+            sys.exit(-1)
+
+    main(url, out_folder)
