e18a70352cbd287b8176e56b81ea853b001a2bd2
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-04-04 16:05:15 +0200

    Adding scripts, updating BASH profile.

---
+++ Shell/backup/backup-music.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -r -t -v -u /Users/michiel/Music/iTunes/iTunes\ Music/Music /Volumes/Family
---
+++ Shell/backup/backup-work.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rsync -r -t -v -u Pieces Paintings Illustrations Fonts Designs Sites /Volumes/michiel/Work
+rsync -r -t -v -u Music/Logic Music/Ardour /Volumes/michiel/Work/Music
